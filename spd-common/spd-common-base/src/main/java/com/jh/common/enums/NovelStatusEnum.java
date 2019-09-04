3b951dca7ff0a7872a27b0bdcdc515a56868bd10
mygit <2601899315@qq.com>
2019-09-04 16:37:58 +0800

    update

--- spd-common/spd-common-base/src/main/java/com/jh/common/enums/NovelStatusEnum.java
+++ spd-common/spd-common-base/src/main/java/com/jh/common/enums/NovelStatusEnum.java
@@ -9,8 +9,9 @@ public enum NovelStatusEnum {
 
     UPLOAD(1,"该小说已上传，正在等待审核"),
     AUDIT_UPPER_SHELF(2, "审核通过，已上架"),
-    // 作者主动下架或解约
+    // 作者主动下架
     LOWER_SHELF(21, "该小说已下架"),
+    // 违规，由工作人员通过后台下架
     VIOLATION_LOWER_SHELF(22, "该小说涉嫌违规，已下架"),
     ;
 
